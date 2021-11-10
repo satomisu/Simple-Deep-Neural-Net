80c9b27eb9e19a94e953582f765d3e9af103d2b2
Satomi Sugaya <satomi@unm.edu>
2021-11-10 10:46:22 -0700

    Commented and cleaned.

--- train_dnn_model.sh
+++ train_dnn_model.sh
@@ -14,13 +14,13 @@ data_path='./DATA/Kuka_swept_volume_data.pkl'
 norm_const=311.17   # Arbitrarily picked from Kuka's sv
 
 # Network parameters
-neur="14 1026 512 256 1"
-acti="None relu relu relu None"
-
+neur="14 1026 512 256 1"          # Number of neurons in each layer, including input + output.
+acti="None relu relu relu None"   # Activation function for each layer, including input.
+                                  # Make sure that the first element (for input) is None.
 # Training parameters
-E=200         # Epochs
-batch=100 # Mini-batch size
+E=2           # Total training epoch
+batch=100     # Mini-batch size
 lrn_rate=0.1  # Learning rate
 
 # Train the model
-python -m train_model $ODIR $ID $R -dfile $data_path -out_dir_path $out_dir_path -lrn_rate $lrn_rate -epoch $E -minibatch $batch -neurons $neur -acti $acti -bias -P -save_eval_at_each_epoch -norm_const $norm_const
+python -m train_model $out_dir_path $ODIR $ID $R -data_file $data_path  -lrn_rate $lrn_rate -epoch $E -minibatch $batch -neurons $neur -acti $acti -bias -P -norm_const $norm_const
